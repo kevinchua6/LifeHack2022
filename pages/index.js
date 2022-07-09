0fd125a4edf5a7855c3dc5572c6424f43b09ac7a
kevin chua <skullspine9@gmail.com>
2022-07-09 18:13:00 +0800

    Add env variables

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Maps from "../components/Maps"
 import { useState } from "react"
 
 const Home = () => {
-  console.log(process.env)
+  console.log(process.env.NEXT_PUBLIC_KEY)
   const [postalCode, setPostalCode] = useState("")
   const [eWaste, setEWaste] = useState([])
   return (
