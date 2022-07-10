3e0dc302cbf4938e5b0d26155edb83de3ec67025
xlzior <wenjun.lye@gmail.com>
2022-07-10 09:13:31 +0800

    Extract out remarks to recyclables list

---
+++ data/remarks-to-recyclables.js
@@ -0,0 +1,42 @@
+const ictEquipment = [
+  "Printers",
+  "Power banks",
+  "Computers, laptops",
+  "Mobile phones, tablets",
+  "Modems, routers",
+  "Network and set-top boxes",
+  "Small TVs",
+  "Desktop monitors",
+];
+
+export const remarksToRecyclableList = {
+  "E-waste accepted: ICT equipment, Batteries and Lamps only": [
+    ...ictEquipment,
+    "Batteries",
+    "Lamps",
+  ],
+  "E-waste accepted: ICT equipment and Batteries only": [
+    ...ictEquipment,
+    "Batteries",
+  ],
+  "E-waste accepted: Batteries and Lamps only": ["Batteries", "Lamps"],
+  "E-waste accepted: Batteries only": ["Batteries"],
+  "E-waste accepted: All regulated consumer products under First Schedule at https://go.gov.sg/prod-def-sl":
+    [
+      "Printers",
+      "Computers, laptops",
+      "Desktop monitors",
+      "Mobile phones, tablets",
+      "Modems, routers",
+      "Network and set-top boxes",
+      "Batteries",
+      "Lamps",
+    ],
+  "E-waste accepted: Non-regulated products only; E.g. Small household appliances, gaming consoles, audio systems, power supplies":
+    [
+      "Small household appliances",
+      "Gaming consoles",
+      "Audio systems",
+      "Power supplies",
+    ],
+};
